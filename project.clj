08902a1ce02de1f2a135a877ad304157d68bfdc4
Adrien Cuisse <adrien.cuisse@epitech.eu>
2025-08-07 14:57:17 +0200

    versions update

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :main rubiks-cube.core
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
-  :plugins [[speclj "3.3.2"]]
+  :dependencies [[org.clojure/clojure "1.12.1"]]
+  :profiles {:dev {:dependencies [[speclj "3.10.0"]]}}
+  :plugins [[speclj "3.10.0"]]
   :test-paths ["spec"])
