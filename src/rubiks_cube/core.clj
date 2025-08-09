ddb3e9dc2c2206e37004f7996228a7f7816fb5f4
Adrien Cuisse <adrien.cuisse@epitech.eu>
2025-08-09 13:37:43 +0200

    doc

--- src/rubiks_cube/core.clj
+++ src/rubiks_cube/core.clj
@@ -9,42 +9,42 @@
 (def yellow \y)
 
 (defn- ^:no-doc create-face
-  "Creates a face with the specified color"
+  "Creates a face with the specified `color`"
   [color]
   (repeat 9 color))
 
 (defn color
-  "Returns the color of a face"
+  "Returns the color of a `face`"
   [face]
   (nth face 5)) ; center cell can't move
 
 (defn front-face
-  "Returns the front face of the cube"
+  "Returns the front face of the `cube`"
   [cube]
   (:front cube))
 
 (defn left-face
-  "Returns the left face of the cube"
+  "Returns the left face of the `cube`"
   [cube]
   (:left cube))
 
 (defn right-face
-  "Returns the right face of the cube"
+  "Returns the right face of the `cube`"
   [cube]
   (:right cube))
 
 (defn top-face
-  "Returns the top face of the cube"
+  "Returns the top face of the `cube`"
   [cube]
   (:top cube))
 
 (defn bottom-face
-  "Returns the bottom face of the cube"
+  "Returns the bottom face of the `cube`"
   [cube]
   (:bottom cube))
 
 (defn back-face
-  "Returns the back face of the cube"
+  "Returns the back face of the `cube`"
   [cube]
   (:back cube))
 
@@ -57,7 +57,13 @@
       (map #(create-face %) (vals colors)))))
 
 (defn- ^:no-doc create-faces-switch-map
-  "Creates a map where keys are source faces, and values their destination"
+  "Creates a map where keys are `source faces`, and values their `destination`
+  Values in the map are the provided keys but rotated once to the right
+
+  ```clojure
+  (create-faces-switch-map [a b c]) ; => {a b, b c, c a}
+  ```
+  "
   [faces-cycle]
   (zipmap
     faces-cycle
@@ -66,38 +72,45 @@
       (first faces-cycle))))
 
 (defn- ^:no-doc rotate
-  "Rotates the cube on itself"
+  "Rotates the `cube` on itself, the first face will be replaced by the second
+  one in `faces-cycle`, the second by the third, [...], the last by the first
+
+  ```clojure
+  (def cube {:a [\\a], :b [\\b], :c [\\c], :d [\\d]})
+  (rotate cube [:a :b :d]) ; => {:a [d], :b [a], :c [c], :d [b]}
+  ```
+  "
   [cube faces-cycle]
   (rename-keys
     cube
     (create-faces-switch-map faces-cycle)))
 
 (defn rotate-left
-  "Rotates the cube to the left"
+  "Rotates the `cube` to the left"
   [cube]
   (rotate cube [:front :left :back :right]))
 
 (defn rotate-right
-  "Rotates the cube to the right"
+  "Rotates the `cube` to the right"
   [cube]
   (rotate cube [:front :right :back :left]))
 
 (defn rotate-up
-  "Rotates the cube up"
+  "Rotates the `cube` up"
   [cube]
   (rotate cube [:front :top :back :bottom]))
 
 (defn rotate-down
-  "Rotates the cube down"
+  "Rotates the `cube` down"
   [cube]
   (rotate cube [:front :bottom :back :top]))
 
 (defn rotate-clockwise
-  "Rotates the cube clockwise"
+  "Rotates the `cube` clockwise"
   [cube]
   (rotate cube [:top :right :bottom :left]))
 
 (defn rotate-anticlockwise
-  "Rotates the cube anticlockwise"
+  "Rotates the `cube` anticlockwise"
   [cube]
   (rotate cube [:top :left :bottom :right]))
